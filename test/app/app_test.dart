7e90862f60af6ba24081b2ce9c1e9868825fcc4a
Eduardo Pires <eduardopires31@gmail.com>
2024-11-03 16:47:53 -0300

    test: ensure repositories injection

--- test/app/app_test.dart
+++ test/app/app_test.dart
@@ -2,10 +2,17 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:coffee_app/app/app.dart';
 import 'package:coffee_app/app/view/app_view.dart';
 import 'package:coffee_app/coffee/cubit/coffee_cubit.dart';
+import 'package:coffee_app/coffee/models/coffee_image.dart';
+import 'package:coffee_app/coffee/repositories/repositories.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockCoffeeCubit extends MockCubit<CoffeeState> implements CoffeeCubit {}
+
+class MockLocalCoffeeRepository extends Mock implements LocalCoffeeRepository {}
+
+class MockRemoteCoffeeRepository extends Mock
+    implements RemoteCoffeeRepository {}
 
 void main() {
   group('App', () {
@@ -36,6 +43,27 @@ void main() {
         ),
       );
       verify(() => mockCoffeeCubit.init()).called(1);
+    });
+    testWidgets('ensure repositories are injected to CoffeeCubit',
+        (tester) async {
+      final mockRemoteCoffeeRepository = MockRemoteCoffeeRepository();
+      final mockLocalCoffeeRepository = MockLocalCoffeeRepository();
+      final sut = App(
+        remoteRepository: mockRemoteCoffeeRepository,
+        localRepository: mockLocalCoffeeRepository,
+      );
+
+      when(mockRemoteCoffeeRepository.fetchRandomImage)
+          .thenAnswer((_) async => const RemoteCoffeeImage('any'));
+      when(mockLocalCoffeeRepository.fetchAllFavorites)
+          .thenAnswer((_) async => [const LocalCoffeeImage('any')]);
+
+      await tester.pumpWidget(sut);
+
+      expect(sut.cubit.localRepository, mockLocalCoffeeRepository);
+      expect(sut.cubit.remoteRepository, mockRemoteCoffeeRepository);
+      verify(mockRemoteCoffeeRepository.fetchRandomImage).called(1);
+      verify(mockLocalCoffeeRepository.fetchAllFavorites).called(1);
     });
   });
 }
